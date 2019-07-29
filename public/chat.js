40c57d281a6ad4fb3ca5d2885bdec76b41a231da
nnijat <nn.aca2019@gmail.com>
2019-07-29 15:23:59 -0500

    Completed Partial sections

--- public/chat.js
+++ public/chat.js
@@ -1,46 +1,49 @@
 `use strict`
 
 // window.onload
+// make a post request to `/clients`. make note, the returned data is not json, it is text()
+// Save the id in a global variable.
+
 let clientId = 0;
 
 window.onload = function () {
-    return fetch('/clients', {
+    fetch("/clients", {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
     })
-        .then(response => response.text())
-        .then(data => clientId = data)
+        .then(res => res.text())
+        .then(newId => clientId = newId)
 }
 
-// Create a text input and a button that says "send"
-function postInputTxt() {
-    let message = document.getElementById('inputMessage').value;
-    console.log("printing text", mess)
+// When the send button is click:
+// retrieve the contents of the text box
+let sendBtn = document.getElementById('sendBtn');
+
+sendBtn.onclick = function send() {
+    let message = document.getElementById("text").value;
+    console.log("Printing current message: ", message)
+
+// create the body as json that looks like `{"clientId":yourid,"text":"value from text box"}`   
     let obj = {
         clientId: clientId,
-        text: text
+        message: message
     }
-    console.log(obj);
-    fetch('/messages', {
+    console.log("printing obj: " + JSON.stringify(obj));
+
+// make a post request to your server at /messages
+    fetch("/messages", {
         method: 'POST',
+        //mode: 'no-cors', // no-cors, cors, *same-origin
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(obj)
     })
-        .then(response => response.json())
+        .then(res => res.json())
         .then(data => console.log(data))
+    document.getElementById("text").innerHTML.value = "";
 }
 
-setInterval(function () {
-    let messagesDiv = document.getElementById('messages')
-    let displayMsg = '';
-    fetch('/messages')
-        .then(response => response.json())
-        .then(data => {
-            data.map(m => {
-                displayMsg += `<div>${m.text}</div>`
-            })
-            messagesDiv.innerHTML = displayMsg;
-        })
-}, 1000);
+
+//learn how to use setInterval to do a fetch call every 1000 miliseconds
+// use this tool to do a fetch call to `/messages` to get the messages array
+// loop the messages array and show the messages in the web page using innerHTML
+
